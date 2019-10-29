d4c291068016de4eea70d107f37016b0ded0b1b6
Nevets44 <sds9278@g.rit.edu>
2019-10-29 16:22:13 -0400

    Added paper submission tables aside from notification

--- Scripts/db_creation_script.sql
+++ Scripts/db_creation_script.sql
@@ -2,6 +2,8 @@
 -- DROP DATABASE IF EXISTS `sam2020`;
 -- CREATE DATABASE `sam2020`;
 
+DROP TABLE IF EXISTS `sam2020`.`paper_authors`;
+DROP TABLE IF EXISTS `sam2020`.`paper`;
 DROP TABLE IF EXISTS `sam2020`.`user`;
 DROP TABLE IF EXISTS `sam2020`.`role`;
 
@@ -25,6 +27,29 @@ CREATE TABLE `sam2020`.`user` (
   KEY `user_role_idx` (`role_id`),
   CONSTRAINT `user_role` FOREIGN KEY (`role_id`) REFERENCES `role` (`role_id`)
 );
+
+CREATE TABLE `sam2020`.`paper` (
+  `paper_id` int(11) NOT NULL AUTO_INCREMENT,
+  `versionNum` int(11) NOT NULL,
+  `document` blob,
+  `submissionDate` datetime DEFAULT NULL,
+  `rating` decimal(2,1) DEFAULT NULL,
+  `contactEmail` varchar(100) DEFAULT NULL,
+  `approvalStatus` tinyint(1) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`paper_id`)
+);
+
+CREATE TABLE `sam2020`.`paper_authors` (
+  `paper_author_id` int(11) NOT NULL AUTO_INCREMENT,
+  `paper_id` int(11) NOT NULL,
+  `author_id` int(11) NOT NULL,
+  `is_contact` tinyint(1) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`paper_author_id`),
+  KEY `paper_id_idx` (`paper_id`),
+  KEY `author_id_idx` (`author_id`),
+  CONSTRAINT `author_id` FOREIGN KEY (`author_id`) REFERENCES `user` (`user_id`),
+  CONSTRAINT `paper_id` FOREIGN KEY (`paper_id`) REFERENCES `paper` (`paper_id`)
+) ;
 
 -- END Create Tables
 
