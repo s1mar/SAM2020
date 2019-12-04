f4c2b5ca27ce960a0427c4cb83ac25938f9ae3a8
Rana Alrubaye <ra9118@rit.edu>
2019-12-04 18:23:08 -0500

    add more reports

--- Models/Reports.cs
+++ Models/Reports.cs
@@ -45,7 +45,7 @@ namespace SAM2020.Models
                 MySqlConnection conn = new MySqlConnection(DBConnect.MyConString);
                 conn.Open();
                 MySqlCommand comm = conn.CreateCommand();
-                comm.CommandText = "select count(*)  as countPaper from paper where reference_name in( select paper_reference_name   from( select paper_reference_name, count(*) as numberOFReviews from review group by paper_reference_name) as t where numberOFReviews!=3)";
+                comm.CommandText = "select count(*)  as countPaper from paper where reference_name not in( select paper_reference_name   from( select paper_reference_name, count(*) as numberOFReviews from review group by paper_reference_name) as t )";
 
                 totalSubmittedPapers = Convert.ToInt32(comm.ExecuteScalar());
                 conn.Close();
@@ -91,6 +91,35 @@ namespace SAM2020.Models
         }
 
 
+       public  int getTotalAlLestOneReviewedPapers()
+        {
+
+            int totalSubmittedPapers = 0;
+
+
+            try
+            {
+
+                MySqlConnection conn = new MySqlConnection(DBConnect.MyConString);
+                conn.Open();
+                MySqlCommand comm = conn.CreateCommand();
+                comm.CommandText = "select count(*) as countPaper from( select paper_reference_name, count(*) as numberOFReviews from review group by paper_reference_name) as t where numberOFReviews>0 and numberOFReviews<3";
+
+                totalSubmittedPapers = Convert.ToInt32(comm.ExecuteScalar());
+                conn.Close();
+
+
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+
+            return totalSubmittedPapers;
+
+        }
 
     }
 }
