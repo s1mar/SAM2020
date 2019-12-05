f6dea3d12e1c2cbbb14426269b4f29471080de27
Leonardo Abreu <leonardoeap@gmail.com>
2019-12-04 22:44:17 -0500

    Add message

--- Constants/DBConnect.cs
+++ Constants/DBConnect.cs
@@ -4,6 +4,7 @@ namespace SAM2020.Models
 {
     public class DBConnect
     {
+      // In order to run this properly, change the credentials with the ones in your mysql instance
         public static String MyConString = "SERVER=localhost;DATABASE=sam2020;UID=root;PASSWORD=PASSWORD;";
     }
 }
